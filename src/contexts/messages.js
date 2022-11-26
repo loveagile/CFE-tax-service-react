81e1bfd299600104fa02122c204fef9b24467a0a
landry <happyplandry@gmail.com>
2022-11-26 08:45:40 -0500

    added: dependent

--- src/contexts/messages.js
+++ src/contexts/messages.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 
 export const MessageContext = React.createContext()
 
 export const MessageProvider = ({ children }) => {
   const [messages, setMessages] = useState([])
 
-  const addMessage = (data) => {
-    const list = [...messages]
-    list.push(data)
-    setMessages(list)
-  }
+  const addMessage = useCallback(
+    (data) => {
+      setMessages([...messages, data])
+    },
+    [messages]
+  )
 
   return (
     <MessageContext.Provider value={{ messages, setMessages, addMessage }}>
