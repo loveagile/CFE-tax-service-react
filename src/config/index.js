30043868b59840a9a6ab433330001c7716619227
landry <happyplandry@gmail.com>
2022-12-21 01:20:56 -0500

    fixed: forgot password

--- src/config/index.js
+++ src/config/index.js
@@ -5,3 +5,6 @@ export const EMAIL_SERVER = `https://api.emailjs.com/api/v1.0/email/send`
 export const YOUR_SERVICE_ID = 'service_shane'
 export const YOUR_TEMPLATE_ID = 'template_vigo78y'
 export const YOUR_PUBLIC_KEY = 'gISgvuP8VVpIKV5oJ'
+
+export const FORGOT_TEMPLATE_ID = 'template_itssvjn'
+export const FROM_EMAIL = 'shaneupkelly@gmail.com'
