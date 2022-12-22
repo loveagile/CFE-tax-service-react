b7c3eb7e7dd552e904c283b299f76eafcbb0288c
landry <happyplandry@gmail.com>
2022-12-22 03:42:23 +0100

    fixed: option on forgot password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Box, Button, Card, TextField, Typography } from '@mui/material'
+import {
+  Box,
+  Button,
+  Card,
+  FormControl,
+  FormControlLabel,
+  RadioGroup,
+  TextField,
+  Typography,
+  Radio,
+} from '@mui/material'
 import { useFormik } from 'formik'
 import emailjs from 'emailjs-com'
 import * as yup from 'yup'
@@ -15,22 +25,22 @@ import {
 } from '../config'
 
 const validationSchema = yup.object({
-  email: yup
-    .string()
-    .email('Type should be email')
-    .required('Email is required'),
-  userid: yup.string().required('UserID is required'),
+  email: yup.string().email('Type should be email'),
+  userid: yup.string(),
 })
 
 const ForgotPassowrd = () => {
   const navigate = useNavigate()
   const initialValues = { email: '', password: '' }
+  const [type, setType] = useState('email')
   const formik = useFormik({
     initialValues: initialValues,
     validationSchema: validationSchema,
     onSubmit: (values) => {
       const { email, userid } = values
-      forgotPassword({ email, userid })
+      let param = email
+      if (type === 'userid') param = userid
+      forgotPassword({ type, param })
         .then(({ data }) => {
           emailjs
             .send(
@@ -56,6 +66,10 @@ const ForgotPassowrd = () => {
     },
   })
 
+  const handleType = (e) => {
+    setType(e.target?.value)
+  }
+
   return (
     <Box
       sx={{ display: 'flex', justifyContent: 'center', paddingTop: '100px' }}
@@ -63,29 +77,53 @@ const ForgotPassowrd = () => {
       <Card
         sx={{
           display: 'flex',
+          flexDirection: 'column',
           padding: '30px',
           height: 'fit-content',
           boxShadow: '0px 2px 10px 4px rgb(0 0 0 / 20%)',
           justifyContent: 'center',
         }}
       >
+        <Typography
+          variant='h5'
+          sx={{
+            display: 'flex',
+            marginBottom: '20px',
+            justifyContent: 'center',
+          }}
+        >
+          Reset password
+        </Typography>
         <form onSubmit={formik.handleSubmit}>
-          <Typography
-            variant='h5'
-            sx={{
-              display: 'flex',
-              marginBottom: '20px',
-              justifyContent: 'center',
-            }}
-          >
-            Reset password
-          </Typography>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby='type-label'
+              value={type}
+              name='type'
+              onChange={handleType}
+            >
+              <FormControlLabel
+                value='email'
+                control={<Radio />}
+                label='I know my email address'
+              />
+              <FormControlLabel
+                value='userid'
+                control={<Radio />}
+                label='I know my user ID'
+              />
+            </RadioGroup>
+          </FormControl>
           <TextField
             id='email'
             type='email'
             label='Email'
             variant='standard'
-            sx={{ width: '100%', marginBottom: 3 }}
+            sx={{
+              width: '100%',
+              marginBottom: 3,
+              display: type === 'email' ? '' : 'none',
+            }}
             onChange={formik.handleChange}
           />
           <TextField
@@ -93,7 +131,11 @@ const ForgotPassowrd = () => {
             type='text'
             label='User ID'
             variant='standard'
-            sx={{ width: '100%', marginBottom: 3 }}
+            sx={{
+              width: '100%',
+              marginBottom: 3,
+              display: type === 'email' ? 'none' : '',
+            }}
             onChange={formik.handleChange}
           />
           <Button
